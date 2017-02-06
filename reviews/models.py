c7eef021448d4ff41609f551408540e02fd8d924
Eshin Kunishima <ek@esh.ink>
2017-02-07 08:03:14 +0900

    add sample Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    review_summary = models.TextField()
+    review_detail_link = models.CharField(max_length=255)
+    localized_by_developer = models.BooleanField()
+    localized_by_community = models.BooleanField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
