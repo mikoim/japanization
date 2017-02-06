0d6e0ec5be224a2d119822a8c0e57f1189ebcb3f
Eshin Kunishima <ek@esh.ink>
2017-02-07 08:02:17 +0900

    add Steam group authentication helper methods

---
+++ japanization/utils.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def privileged_members(group_name: str) -> list:
+    res = requests.get('http://steamcommunity.com/groups/{:s}/members'.format(group_name), {
+        'content_only': True
+    })
+
+    if res.status_code is not 200:
+        raise SteamScraperError('Can\'t retrieve group members page')
+
+    members = parse_privileged_member_names(res.text)
+
+    return [steam_id_by_name(m) for m in members]
+
+
+def steam_id_by_name(name: str) -> int:
+    res = requests.get('http://steamcommunity.com/id/{:s}'.format(name), {
+        'xml': 1
+    })
+
+    if res.status_code is not 200:
+        raise SteamScraperError('Can\'t retrieve user profile')
+
+    return parse_steam_id(res.text)
+
+
+def parse_steam_id(xml: str) -> int:
+    root = BeautifulSoup(xml, "lxml-xml")
+    return int(root.find('steamID64').contents[0])
+
+
+def parse_privileged_member_names(html: str) -> list:
+    root = BeautifulSoup(html, 'lxml')
+    members_child = root.find_all('div', class_='rank_icon')
+    members = [m.parent.find('a', class_='linkFriend').get('href').split('/')[-1] for m in members_child]
+    return members
+
+
+class SteamScraperError(Exception):
+    pass
