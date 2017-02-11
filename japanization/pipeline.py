00a04d27764040ae793faa66b9b699da564e251b
Eshin Kunishima <ek@esh.ink>
2017-02-11 15:09:09 +0900

    add pipeline for Steam group authentication

--- japanization/pipeline.py
+++ japanization/pipeline.py
@@ -1,2 +1,18 @@
+from .utils import privileged_members
+from django.contrib.auth.models import Permission
+
+
 def check_group_status(backend, user, response, *args, **kwargs):
-    print(response)
+    editor_perms = [
+        Permission.objects.get(codename='add_review'),
+        Permission.objects.get(codename='change_review')
+    ]
+
+    if kwargs['uid'] in privileged_members('japanization'):
+        user.is_staff = True
+        user.user_permissions.add(*editor_perms)
+        user.save()
+    else:
+        user.is_staff = False
+        user.user_permissions.remove(*editor_perms)
+        user.save()
--- japanization/utils.py
+++ japanization/utils.py
@@ -1,8 +1,20 @@
 import requests
+from django.core.cache import cache
 from bs4 import BeautifulSoup
 
 
 def privileged_members(group_name: str) -> list:
+    key = 'privileged_members_{:s}'.format(group_name)
+    members = cache.get('privileged_members_{:s}'.format(group_name))
+
+    if not members:
+        members = privileged_members_raw(group_name)
+        cache.set(key, members, 900)
+
+    return members
+
+
+def privileged_members_raw(group_name: str) -> list:
     res = requests.get('http://steamcommunity.com/groups/{:s}/members'.format(group_name), {
         'content_only': True
     })
@@ -12,10 +24,21 @@ def privileged_members(group_name: str) -> list:
 
     members = parse_privileged_member_names(res.text)
 
-    return [steam_id_by_name(m) for m in members]
+    return [steam_id_by_name_raw(m) for m in members]
 
 
-def steam_id_by_name(name: str) -> int:
+def steam_id_by_name(name: str) -> list:
+    key = 'steam_id_by_name_{:s}'.format(name)
+    steam_id = cache.get(key)
+
+    if not steam_id:
+        steam_id = steam_id_by_name(name)
+        cache.set(key, steam_id, 604800)
+
+    return steam_id
+
+
+def steam_id_by_name_raw(name: str) -> str:
     res = requests.get('http://steamcommunity.com/id/{:s}'.format(name), {
         'xml': 1
     })
