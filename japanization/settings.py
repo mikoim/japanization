a95d74105192eda6f1b734c5c7a3b226f932610f
Eshin Kunishima <ek@esh.ink>
2017-02-13 22:28:25 +0900

    add support for DATABASE_URL

--- japanization/settings.py
+++ japanization/settings.py
@@ -103,6 +103,14 @@ DATABASES = {
     }
 }
 
+# Use external database if DATABASE_URL environment variables is set
+
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
