c3a10f2e57c12153433b3ba69a2afff688ffd21a
Eshin Kunishima <ek@esh.ink>
2017-02-14 15:35:14 +0900

    move edit page

--- japanization/urls.py
+++ japanization/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from django.views.generic import TemplateView
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('social_django.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
-
-    url(r'^$', TemplateView.as_view(template_name='reviews/index.html')),
     url('', include('reviews.urls'))
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView, ListView, RedirectView
 from rest_framework.routers import DefaultRouter
+
 from .views import ReviewViewSet
+from .models import Review
 
 router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/sample')),
     url(r'^api/', include(router.urls)),
+    url(r'^sample$', ListView.as_view(model=Review)),
+    url(r'^edit$', TemplateView.as_view(template_name='reviews/edit.html')),
 ]
