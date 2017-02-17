f261fc738dfc44909ee2e0e02c860e7566f40147
Eshin Kunishima <ek@esh.ink>
2017-02-17 17:44:59 +0900

    show some fields in admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Review
 
-admin.site.register(Review)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'updated_at', 'steam_app_id', 'name', 'localized_by_developer', 'localized_by_community', 'published', 'invalid'
+    )
+    list_filter = [
+        'updated_at', 'localized_by_developer', 'localized_by_community', 'published', 'invalid'
+    ]
+admin.site.register(Review, ReviewAdmin)
