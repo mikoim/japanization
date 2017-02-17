fcbe7d59a085bc957491852e1f1693747d393bc9
Eshin Kunishima <ek@esh.ink>
2017-02-17 14:50:04 +0900

    add sample page

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateView, ListView, RedirectView
+from django.views.generic import TemplateView, ListView
 from rest_framework.routers import DefaultRouter
 
 from .models import Review
@@ -9,8 +9,8 @@ router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/sample'), name='reviews-index'),
+    url(r'^$', ListView.as_view(model=Review, template_name='reviews/index_list.html'), name='reviews-index'),
     url(r'^api/', include(router.urls), name='reviews-api'),
-    url(r'^sample$', ListView.as_view(model=Review), name='reviews-sample'),
+    url(r'^sample$', ListView.as_view(model=Review, template_name='reviews/sample_list.html'), name='reviews-sample'),
     url(r'^edit$', TemplateView.as_view(template_name='reviews/edit.html'), name='reviews-edit'),
 ]
