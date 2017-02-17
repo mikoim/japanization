e4141b021219bbcc2af8d64ab1ddbd1b8b839055
Eshin Kunishima <ek@esh.ink>
2017-02-17 17:46:30 +0900

    add custom actions for managing Review model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib import messages
 
 from .models import Review
+from .utils import SteamStore, SteamException
 
 
 class ReviewAdmin(admin.ModelAdmin):
@@ -10,4 +12,51 @@ class ReviewAdmin(admin.ModelAdmin):
     list_filter = [
         'updated_at', 'localized_by_developer', 'localized_by_community', 'published', 'invalid'
     ]
+    actions = ['mark_as_draft', 'mark_as_published', 'sync_with_steam']
+
+    def mark_as_draft(self, request, queryset):
+        rows_updated = queryset.update(published=False)
+        if rows_updated == 1:
+            message_bit = '1 review was'
+        else:
+            message_bit = '%s reviews were' % rows_updated
+        self.message_user(request, '%s successfully marked as draft.' % message_bit)
+
+    def mark_as_published(self, request, queryset):
+        rows_updated = queryset.update(published=True)
+        if rows_updated == 1:
+            message_bit = '1 review was'
+        else:
+            message_bit = '%s reviews were' % rows_updated
+        self.message_user(request, '%s successfully marked as published.' % message_bit)
+
+    def sync_with_steam(self, request, queryset):
+        store = SteamStore()
+        success = 0
+
+        for row in queryset:
+            data = None
+            app_id = row.steam_app_id
+
+            try:
+                data = store.appdetails(app_id)
+            except SteamException as e:
+                self.message_user(request, 'Steam AppID {:d} is invalid. ({:s})'.format(app_id, str(e)), messages.ERROR)
+                row.invalid = True
+
+            if data:
+                row.name = store.name(data)
+                row.localized_by_developer = store.is_support_japanese(data)
+                row.invalid = False
+                success += 1
+
+            row.save()
+
+        if success >= 1:
+            if success == 1:
+                message_bit = '1 review was'
+            else:
+                message_bit = '%s reviews were' % success
+            self.message_user(request, '%s successfully synced with Steam.' % message_bit)
+
 admin.site.register(Review, ReviewAdmin)
