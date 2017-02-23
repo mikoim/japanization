ab562f2e84700c5e1dd14220a00f85128e3680f6
Eshin Kunishima <ek@esh.ink>
2017-02-24 00:27:01 +0900

    add localization_status field in API

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -7,6 +7,7 @@ class ReviewSerializer(ModelSerializer):
     class Meta:
         model = Review
         fields = (
-            'steam_app_id', 'name', 'review_summary', 'review_detail_link',
-            'localized_by_developer', 'localized_by_community'
+            'steam_app_id', 'name',
+            'review_summary', 'review_detail', 'review_detail_link',
+            'localization_status', 'localized_by_developer', 'localized_by_community'
         )
