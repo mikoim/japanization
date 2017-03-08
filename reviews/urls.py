7dba7cdd0251e0f85054a087cc4aff2393ab3e9c
Eshin Kunishima <ek@esh.ink>
2017-03-08 16:08:22 +0900

    enable list view caching

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.decorators.cache import cache_page as cp
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
@@ -8,9 +9,9 @@ router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
-    url(r'^$', ReviewView.as_view(template_name='reviews/index_list.html'), name='reviews-index'),
+    url(r'^$', cp(60 * 5)(ReviewView.as_view(template_name='reviews/index_list.html')), name='reviews-index'),
     url(r'^api/', include(router.urls), name='reviews-api'),
-    url(r'^manual$', ReviewView.as_view(template_name='reviews/manual.html'), name='reviews-manual'),
-    url(r'^sample$', ReviewView.as_view(template_name='reviews/sample_list.html'), name='reviews-sample'),
+    url(r'^manual$', cp(60 * 60)(ReviewView.as_view(template_name='reviews/manual.html')), name='reviews-manual'),
+    url(r'^sample$', cp(60 * 5)(ReviewView.as_view(template_name='reviews/sample_list.html')), name='reviews-sample'),
     url(r'^edit$', TemplateView.as_view(template_name='reviews/edit.html'), name='reviews-edit'),
 ]
